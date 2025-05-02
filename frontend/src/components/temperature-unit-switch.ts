d68810539fdd4d8f6998e6a1b0b5f57d5c4fe5ea
vinicivs-rocha <vinicivsdev@gmail.com>
2025-05-02 16:29:01 -0300

    style: add prettier

--- frontend/src/components/temperature-unit-switch.ts
+++ frontend/src/components/temperature-unit-switch.ts
@@ -1,53 +1,53 @@
-import {styled, Switch} from "@mui/material";
+import { styled, Switch } from "@mui/material";
 
 export const TemperatureUnitSwitch = styled(Switch)(({ theme }) => ({
-    width: 62,
-    height: 34,
-    padding: 7,
-    '& .MuiSwitch-switchBase': {
-        margin: 1,
-        padding: 0,
-        transform: 'translateX(6px)',
-        '&.Mui-checked': {
-            color: '#fff',
-            transform: 'translateX(22px)',
-            '& .MuiSwitch-thumb:before': {
-                backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text x="10" y="70" font-size="60" font-family="Arial" fill="${encodeURIComponent('#fff')}">°F</text></svg>')`,
-            },
-            '& + .MuiSwitch-track': {
-                opacity: 1,
-                backgroundColor: '#aab4be',
-                ...theme.applyStyles('dark', {
-                    backgroundColor: '#8796A5',
-                }),
-            },
-        },
-    },
-    '& .MuiSwitch-thumb': {
-        backgroundColor: '#001e3c',
-        width: 32,
-        height: 32,
-        '&::before': {
-            content: "''",
-            position: 'absolute',
-            width: '100%',
-            height: '100%',
-            left: 0,
-            top: 0,
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-            backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text x="10" y="70" font-size="60" font-family="Arial" fill="${encodeURIComponent('#fff')}">°C</text></svg>')`,
-        },
-        ...theme.applyStyles('dark', {
-            backgroundColor: '#003892',
-        }),
-    },
-    '& .MuiSwitch-track': {
+  width: 62,
+  height: 34,
+  padding: 7,
+  "& .MuiSwitch-switchBase": {
+    margin: 1,
+    padding: 0,
+    transform: "translateX(6px)",
+    "&.Mui-checked": {
+      color: "#fff",
+      transform: "translateX(22px)",
+      "& .MuiSwitch-thumb:before": {
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text x="10" y="70" font-size="60" font-family="Arial" fill="${encodeURIComponent("#fff")}">°F</text></svg>')`,
+      },
+      "& + .MuiSwitch-track": {
         opacity: 1,
-        backgroundColor: '#aab4be',
-        borderRadius: 20 / 2,
-        ...theme.applyStyles('dark', {
-            backgroundColor: '#8796A5',
+        backgroundColor: "#aab4be",
+        ...theme.applyStyles("dark", {
+          backgroundColor: "#8796A5",
         }),
+      },
     },
+  },
+  "& .MuiSwitch-thumb": {
+    backgroundColor: "#001e3c",
+    width: 32,
+    height: 32,
+    "&::before": {
+      content: "''",
+      position: "absolute",
+      width: "100%",
+      height: "100%",
+      left: 0,
+      top: 0,
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",
+      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text x="10" y="70" font-size="60" font-family="Arial" fill="${encodeURIComponent("#fff")}">°C</text></svg>')`,
+    },
+    ...theme.applyStyles("dark", {
+      backgroundColor: "#003892",
+    }),
+  },
+  "& .MuiSwitch-track": {
+    opacity: 1,
+    backgroundColor: "#aab4be",
+    borderRadius: 20 / 2,
+    ...theme.applyStyles("dark", {
+      backgroundColor: "#8796A5",
+    }),
+  },
 }));
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,20 +1,20 @@
-import {Box} from "@mui/material";
-import {useSearchParams} from "react-router";
+import { Box } from "@mui/material";
+import { useSearchParams } from "react-router";
 
 export function Home() {
-    const [searchParams] = useSearchParams()
-    return (
-        <Box mt={4} height={500} borderRadius={2} overflow="hidden" boxShadow={1}>
-            <iframe
-                title="map"
-                width="100%"
-                height="100%"
-                style={{border: 0}}
-                src={`https://www.google.com/maps?q=${encodeURIComponent(
-                    searchParams.get('location') ?? ''
-                )}&output=embed`}
-                allowFullScreen
-            />
-        </Box>
-    )
+  const [searchParams] = useSearchParams();
+  return (
+    <Box mt={4} height={500} borderRadius={2} overflow="hidden" boxShadow={1}>
+      <iframe
+        title="map"
+        width="100%"
+        height="100%"
+        style={{ border: 0 }}
+        src={`https://www.google.com/maps?q=${encodeURIComponent(
+          searchParams.get("location") ?? "",
+        )}&output=embed`}
+        allowFullScreen
+      />
+    </Box>
+  );
 }
