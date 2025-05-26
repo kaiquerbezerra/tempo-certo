e37c07fb325c3d6b35d21ff00d5b8e4530f472c6
vinicivs-rocha <vinicivsdev@gmail.com>
2025-05-26 14:07:16 -0300

    feat: add data fetching

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,104 +1,140 @@
 import { useSearchParams } from "react-router";
 import { Box, Grid, Typography } from "@mui/material";
-import { CloudRain, CloudSun, Cloudy, Sun } from "lucide-react";
+import {
+  Cloud,
+  CloudDrizzle,
+  CloudFog,
+  CloudHail,
+  CloudLightning,
+  CloudRain,
+  CloudSnow,
+  CloudSun,
+  LucideProps,
+  Snowflake,
+  Sun,
+} from "lucide-react";
 import dayjs from "dayjs";
 import { LineChart } from "@mui/x-charts";
+import { useEffect, useState } from "react";
+import * as React from "react";
 
-enum WeatherState {
-  PartlyCloudy = "partly-cloudy",
-  Cloudy = "cloudy",
-  Rainy = "rainy",
-  Sunny = "sunny",
-}
+const weatherStateIconLookup: {
+  [code: number]: React.ForwardRefExoticComponent<
+    Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>
+  >;
+} = {
+  113: Sun,
+  116: CloudSun,
+  119: Cloud,
+  122: Cloud,
+  143: CloudFog,
+  176: CloudDrizzle,
+  179: CloudSnow,
+  182: CloudHail,
+  185: CloudDrizzle,
+  200: CloudLightning,
+  227: CloudSnow,
+  230: CloudSnow,
+  248: CloudFog,
+  260: CloudFog,
+  263: CloudDrizzle,
+  266: CloudDrizzle,
+  281: CloudDrizzle,
+  284: CloudDrizzle,
+  293: CloudRain,
+  296: CloudRain,
+  299: CloudRain,
+  302: CloudRain,
+  305: CloudRain,
+  308: CloudRain,
+  311: CloudRain,
+  314: CloudRain,
+  317: CloudHail,
+  320: CloudHail,
+  323: CloudSnow,
+  326: CloudSnow,
+  329: CloudSnow,
+  332: CloudSnow,
+  335: CloudSnow,
+  338: CloudSnow,
+  350: Snowflake,
+  353: CloudDrizzle,
+  356: CloudRain,
+  359: CloudRain,
+  362: CloudHail,
+  365: CloudHail,
+  368: CloudSnow,
+  371: CloudSnow,
+  374: Snowflake,
+  377: Snowflake,
+  386: CloudLightning,
+  389: CloudLightning,
+  392: CloudLightning,
+  395: CloudLightning,
+};
 
-const weatherStateIconLookup = {
-  [WeatherState.PartlyCloudy]: CloudSun,
-  [WeatherState.Cloudy]: Cloudy,
-  [WeatherState.Rainy]: CloudRain,
-  [WeatherState.Sunny]: Sun,
+type WeatherForecastData = {
+  country: string;
+  weatherState: number;
+  temperature: number;
+  humidity: number;
+  windSpeed: number;
+  weatherPreview: {
+    time: string;
+    weatherState: number;
+    temperature: number;
+  }[];
+  forecastPreview: {
+    date: string;
+    min: number;
+    max: number;
+    weatherState: number;
+  }[];
+  weatherSeries: {
+    time: string;
+    temperature: number;
+  }[];
 };
 
 export function Dashboard() {
   const [searchParams] = useSearchParams();
-  const locationParam = searchParams.get("location");
+  const locationParam = searchParams.get("location") || "";
   const startsAtParam = searchParams.get("startsAt");
   const endsAtParam = searchParams.get("endsAt");
-  const startsAt = startsAtParam ? dayjs(startsAtParam) : null;
-  const endsAt = endsAtParam ? dayjs(endsAtParam) : null;
+  const isUsingFahrenheitParam = searchParams.get("isUsingFahrenheit");
 
-  const {
-    location,
-    temperature,
-    humidity,
-    windSpeed,
-    weatherState,
-    weatherPreview,
-  } = {
-    weatherState: Object.values(WeatherState)[Math.floor(Math.random() * 4)],
-    location: {
-      name: locationParam!,
-      country: "Inglaterra",
-    },
-    temperature: 20,
-    humidity: 24,
-    windSpeed: 13,
-    weatherPreview: Array.from({ length: 10 }).map((_, i) => ({
-      time: startsAt!.add(i, "hour"),
-      weatherState: Object.values(WeatherState)[Math.floor(Math.random() * 4)],
-      temperature: Math.floor(Math.random() * 30),
-    })),
-  };
+  const [weatherForecastData, setWeatherForecastData] =
+    useState<WeatherForecastData | null>();
 
-  const WeatherStateIcon = weatherStateIconLookup[weatherState];
+  useEffect(() => {
+    const fetchWeatherForecastData = async () => {
+      const response = await fetch(
+        `http://localhost:8000/api/weather-forecast?${new URLSearchParams({
+          location: locationParam,
+          startsAt: startsAtParam
+            ? dayjs(startsAtParam).toISOString()
+            : dayjs().toISOString(),
+          endsAt: endsAtParam
+            ? dayjs(endsAtParam).toISOString()
+            : dayjs().toISOString(),
+          isUsingFahrenheit: isUsingFahrenheitParam
+            ? isUsingFahrenheitParam
+            : "false",
+        }).toString()}`,
+      );
+      const data = await response.json();
+      setWeatherForecastData(data);
+    };
 
-  const daysDiff = (startsAt && endsAt?.diff(startsAt, "day")) || 0;
-  const daysDiffFromEight = Math.floor(8 - daysDiff);
-  const moreThanEightDays = daysDiffFromEight < 0;
-  const daysToAdd = moreThanEightDays ? 0 : daysDiffFromEight;
+    fetchWeatherForecastData().then(() => {});
+  }, [startsAtParam, endsAtParam, locationParam, isUsingFahrenheitParam]);
 
-  const filtersDaysSeries =
-    startsAt &&
-    endsAt &&
-    Array.from({ length: daysDiff + 1 }).map((_, i) => startsAt.add(i, "day"));
-  const complementaryDaysSeries =
-    startsAt &&
-    endsAt &&
-    Array.from({ length: daysToAdd - 1 })
-      .map((_, i) => startsAt.subtract(i + 1, "day"))
-      .reverse();
-  const daysSeries =
-    startsAt && endsAt && complementaryDaysSeries!.concat(filtersDaysSeries!);
-  const forecastSeries =
-    daysSeries &&
-    daysSeries.map((item) => ({
-      date: item,
-      min: Math.floor(Math.random() * 30),
-      max: Math.floor(Math.random() * 30),
-      weatherState: Object.values(WeatherState)[Math.floor(Math.random() * 4)],
-    }));
+  if (!weatherForecastData) {
+    return <div>Loading...</div>;
+  }
 
-  const hoursDiff = (startsAt && endsAt?.diff(startsAt, "hour")) || 0;
-  const hoursDiffFromTen = Math.floor(10 - hoursDiff);
-  const moreThanTenHours = hoursDiffFromTen < 0;
-  const hoursToAdd = moreThanTenHours ? 0 : hoursDiffFromTen;
-
-  const filtersHoursSeries =
-    startsAt &&
-    endsAt &&
-    Array.from({ length: hoursDiff + 1 }).map((_, i) =>
-      startsAt.add(i, "hour"),
-    );
-  const complementaryHoursSeries =
-    startsAt &&
-    endsAt &&
-    Array.from({ length: hoursToAdd - 1 })
-      .map((_, i) => startsAt.subtract(i + 1, "hour"))
-      .reverse();
-  const hoursSeries =
-    startsAt && endsAt && complementaryHoursSeries!.concat(filtersHoursSeries!);
-  const weatherSeries =
-    hoursSeries &&
-    Array.from({ length: hoursSeries.length }).map(() => Math.random() * 30);
+  const WeatherStateIcon =
+    weatherStateIconLookup[weatherForecastData.weatherState];
 
   return (
     <Grid flexGrow={1} container py={1} gap={8}>
@@ -116,9 +152,11 @@ export function Dashboard() {
                 spacing={1}
               >
                 <Typography fontSize={24} fontWeight="medium">
-                  {location.name}
+                  {locationParam}
                 </Typography>
-                <Typography fontSize={14}>{location.country}</Typography>
+                <Typography fontSize={14}>
+                  {weatherForecastData.country}
+                </Typography>
               </Grid>
               <Grid
                 size={2}
@@ -129,7 +167,7 @@ export function Dashboard() {
                 spacing={1}
               >
                 <Typography fontSize={24} fontWeight="medium">
-                  {temperature}º
+                  {weatherForecastData.temperature}º
                 </Typography>
                 <Typography fontSize={14}>Temperatura</Typography>
               </Grid>
@@ -142,19 +180,19 @@ export function Dashboard() {
                 spacing={1}
               >
                 <Typography fontSize={24} fontWeight="medium">
-                  {humidity}%
+                  {weatherForecastData.humidity}%
                 </Typography>
                 <Typography fontSize={14}>Umidade</Typography>
               </Grid>
               <Grid size={3}>
                 <Typography fontSize={24} fontWeight="medium">
-                  {windSpeed}km/h
+                  {weatherForecastData.windSpeed}km/h
                 </Typography>
                 <Typography fontSize={14}>Velocidade do vento</Typography>
               </Grid>
             </Grid>
             <Grid container direction="row" justifyContent="space-between">
-              {weatherPreview.map((item, index) => {
+              {weatherForecastData.weatherPreview.map((item, index) => {
                 const WeatherStateIcon =
                   weatherStateIconLookup[item.weatherState];
                 return (
@@ -172,7 +210,7 @@ export function Dashboard() {
                       alignItems="center"
                     >
                       <Typography fontSize={10} fontWeight="bold">
-                        {item.time.format("hh a")}
+                        {dayjs(item.time).format("hh a")}
                       </Typography>
                       <WeatherStateIcon fill="black" />
                       <Typography fontSize={10} fontWeight="bold">
@@ -185,30 +223,33 @@ export function Dashboard() {
             </Grid>
           </Grid>
         </Box>
-        {hoursSeries && weatherSeries && (
-          <LineChart
-            sx={{ flexGrow: 1 }}
-            xAxis={[
-              {
-                scaleType: "time",
-                data: hoursSeries,
-                valueFormatter: (value) => dayjs(value).format("D - hh a"),
-              },
-            ]}
-            yAxis={[
-              {
-                valueFormatter: (value: number) => `${value.toFixed(1)}º`,
-              },
-            ]}
-            series={[
-              {
-                data: weatherSeries,
-                valueFormatter: (value) =>
-                  value !== null ? `${value.toFixed(1)}º` : "",
-              },
-            ]}
-          />
-        )}
+
+        <LineChart
+          sx={{ flexGrow: 1 }}
+          xAxis={[
+            {
+              scaleType: "time",
+              data: weatherForecastData.weatherSeries.map(({ time }) =>
+                dayjs(time),
+              ),
+              valueFormatter: (value) => dayjs(value).format("D - hh a"),
+            },
+          ]}
+          yAxis={[
+            {
+              valueFormatter: (value: number) => `${value.toFixed(1)}º`,
+            },
+          ]}
+          series={[
+            {
+              data: weatherForecastData.weatherSeries.map(
+                ({ temperature }) => temperature,
+              ),
+              valueFormatter: (value) =>
+                value !== null ? `${value.toFixed(1)}º` : "",
+            },
+          ]}
+        />
       </Grid>
       <Grid container size={4} flexGrow={1}>
         <Box
@@ -221,7 +262,7 @@ export function Dashboard() {
           flexGrow={1}
         >
           <Typography fontSize={28}>Previsão para {locationParam}</Typography>
-          {forecastSeries?.map((item, index) => {
+          {weatherForecastData.forecastPreview.map((item, index) => {
             const WeatherStateIcon = weatherStateIconLookup[item.weatherState];
             return (
               <Box
@@ -240,7 +281,9 @@ export function Dashboard() {
                   </Typography>
                 </Box>
                 <Box>
-                  <Typography>{item.date.format("D MMMM, ddd")}</Typography>
+                  <Typography>
+                    {dayjs(item.date).format("D MMMM, ddd")}
+                  </Typography>
                 </Box>
               </Box>
             );
